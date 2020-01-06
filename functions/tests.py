67b7d350dfa6bbb6af1eb2250be5b281adc5cc39
gtc081 <34279199+gtc081@users.noreply.github.com>
2020-01-06 20:17:59 -0300

    Update tests.py

--- functions/tests.py
+++ functions/tests.py
@@ -49,9 +49,18 @@ class TestFunctions(unittest.TestCase):
         
     def test_create_contract_no_valor(self):
         self.assertEqual('ERRO: "Valor do empréstimo" é um campo obrigatório', self.funcao.create(client,TABELA_CONTRATOS,TABELA_CRIACAO,None,json.loads('{"Nome":"valor","Email":"valor","CPF":"valor"}')))
+      
+    def test_analyze_finalize_no_requirements(self):
+        self.assertEqual("Erro: para finalizar um contrato, faça o upload de um arquivo para CNH ou para o CPF.", self.funcao.can_finalize(client,TABELA_UPLOAD,'id_qualquer'))
         
     def test_can_finalize_no_requirements(self):
         self.assertEqual(False, self.funcao.can_finalize(client,TABELA_UPLOAD,'id_qualquer'))
+        
+    def test_create_file_no_requirements(self):
+        self.assertEqual('Erro: "Caminho do arquivo" não especificado no JSON de entrada', self.funcao.can_finalize(client,s3,TABELA_CONTRATOS,TABELA_UPLOAD,BUCKET,json.loads('{"Status_errado":"qualquer"}'),"estado_qualquer",'id_qualquer'))    
+       
+    def test_create_file_wrong_requirements(self):
+        self.assertEqual('JSON inválido. O formato correto deve ser: {"Caminho do arquivo":"CAMINHO_ABSOLUTO_DO_ARQUIVO_COM_EXTENSÃO"}<br><br> Verifique se o caminho está correto', self.funcao.can_finalize(client,s3,TABELA_CONTRATOS,TABELA_UPLOAD,BUCKET,json.loads('{"Caminho do arquivo":"qualquer"}'),"estado_qualquer",'id_qualquer'))   
         
     def test_finalize_no_status(self):
         self.assertEqual('Erro: "Status" não especificado no JSON de entrada', self.funcao.finalize(client,TABELA_CONTRATOS,'id_qualquer',json.loads('{"Status_errado":"qualquer"}')))
